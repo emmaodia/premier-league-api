96508600f7271ca8518b6722079ed13ec85a6bb9
emmaodia <odia.emma@gmail.com>
2020-02-06 16:50:07 +0100

    Created server.js file.

--- app.js
+++ app.js
@@ -4,7 +4,5 @@ const bodyParser = require("body-parser");
 
 app.get('/', (req, res) => res.json({msg: "Welcome, Nerd!"}))
 
-const port = 3000;
-app.listen(port, () => console.log(`App running on ${port}!`))
 
 module.exports = app;
