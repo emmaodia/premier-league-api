90681adb7c690ee9646db608dc7af3f602899f16
emmaodia <odia.emma@gmail.com>
2020-02-07 10:41:59 +0100

    Feature: Implemented Sign Up

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const Admin = require('../models/admin');
+const bcrypt = require('bcryptjs');
 
 router.get('/', async(req, res) => {
-    //res.status(200).json({msg: "This is JSON!"});
+    try {
+         //res.status(200).json({msg: "This is JSON!"});
     const admin = await Admin.find()
     //res.status(200).json(admin)
     const response = await admin.map(admin => {
@@ -13,15 +15,41 @@ router.get('/', async(req, res) => {
         }
     })
     res.status(200).json(response)
+    } catch (error) {
+        console.log(error)
+    }
+   
 })
 
-router.post('/create', async(req, res) => {
-    const {email, password} = req.body;
-    const admin = new Admin({email, password});
+router.post('/signup', async(req, res) => {
+    try {
+        const {email, password} = req.body;
+        const hash = bcrypt.hash(password, 10, (err, hash) => {
+            if (err) {
+                return res.status(500).json({
+                  error: err
+                });
+            }
+        })
+    
+        const admin = await Admin.find({email});
+        
+        if (admin.length >= 1) {
+            return res.status(409).json({
+              message: "Email already exists!"
+            });
+          } else {
+           const result = new Admin({email, password: hash});
+           await result.save();
 
-    const response = await admin.save();
-
-    res.status(200).json(response)
+           console.log(result);
+            res.status(200).json(result)
+          }
+       
+    } catch (error) {
+        console.log(error)
+    }
+   
 })
 
 module.exports = router;
