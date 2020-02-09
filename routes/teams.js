a3f8c54cf19940d0ab90a9a06f19ae781de40f3c
emmaodia <odia.emma@gmail.com>
2020-02-09 02:20:55 +0100

    Implemented POST endpoint for teams

--- routes/teams.js
+++ routes/teams.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const Team = require('../models/teams');
 
 router.get('/', async(req, res) => { console.log("woeking")});
+
+router.post('/create', async(req, res) => {
+    const { name, city, coach } = req.body;
+    const team = new Team({ name, city, coach });
+
+    const response = await team.save();
+
+    res.status(200).json(response);
+});
 
 module.exports = router;
