1f602a9092577897b08a89dca5ed70b3679986d8
emmaodia <odia.emma@gmail.com>
2020-02-09 02:25:34 +0100

    Implemented GET ALL endpoint for teams

--- routes/teams.js
+++ routes/teams.js
@@ -2,7 +2,23 @@ const express = require("express");
 const router = express.Router();
 const Team = require('../models/teams');
 
-router.get('/', async(req, res) => { console.log("woeking")});
+router.get('/', async(req, res) => { 
+
+    const team = await Team.find();
+
+    const response = await team.map(team => {
+        return {
+            _id: team._id,
+            name: team.name,
+            city: team.city,
+            coach: team.coach
+        }
+    })
+    res.status(200).json(response)
+    console.log(response)
+
+
+});
 
 router.post('/create', async(req, res) => {
     const { name, city, coach } = req.body;
