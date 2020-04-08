974630ca2817e6f6fedbc20961e842e0823dc628
emmaodia <odia.emma@gmail.com>
2020-04-08 07:02:15 +0100

    Set Up Redis.

--- app.js
+++ app.js
@@ -3,10 +3,25 @@ const app = express();
 const bodyParser = require("body-parser");
 const dbConfig = require('./dbConfig');
 
+//Redis Configuration
+const redis = require("redis");
+const port_redis = process.env.PORT || 6379;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => res.json({msg: "Welcome, Nerd!"}))
 
+
+//configure redis client on port 6379
+const redis_client = redis.createClient(port_redis);
+
+redis_client.on('connect', function() {
+    console.log('Redis client connected');
+});
+
+redis_client.on('error', function (err) {
+    console.log('Something went wrong ' + err);
+});
 
 module.exports = app;
