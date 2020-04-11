71f05037b22a67dc69e291e3d3d42b6914127241
emmaodia <odia.emma@gmail.com>
2020-04-11 15:34:46 +0100

    Set up dotenv config.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const dbConfig = require('./dbConfig');
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dbConfig = express.Router();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const url = 'mongodb://localhost:27017/premier-league-api';
 
 mongoose.Promise = global.Promise;
