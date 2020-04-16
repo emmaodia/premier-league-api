3324f44615a63bf59b1af9df57e3f52ef7e87f86
emmaodia <odia.emma@gmail.com>
2020-04-16 21:42:23 +0100

    ...

--- redis.js
+++ redis.js
@@ -1,14 +1,14 @@
-const redis = require('redis');
-const port_redis = process.env.REDIS_URL;
+// const redis = require('redis');
+// const port_redis = process.env.REDIS_URL;
 
-const redis_client = redis.createClient(port_redis);
+// const redis_client = redis.createClient(port_redis);
 
-redis_client.on('connect', function() {
-    console.log('Redis client connected');
-});
+// redis_client.on('connect', function() {
+//     console.log('Redis client connected');
+// });
 
 // redis_client.on('error', function (err) {
 //     console.log('Something went wrong ' + err);
 // });
 
-exports.redis_client = redis.createClient(port_redis);
+// exports.redis_client = redis.createClient(port_redis);
